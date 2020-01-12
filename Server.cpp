881cf9602334044894e07efed08fb5cf6a142c1a
amittwito <amit6991@gmail.com>
2020-01-12 17:33:01 +0200

    Opened new c++ 14 project for mile stone2

---
+++ Server.cpp
@@ -0,0 +1,5 @@
+//
+// Created by amit on 12/01/2020.
+//
+
+#include "Server.h"
---
+++ Server.h
@@ -0,0 +1,12 @@
+//
+// Created by amit on 12/01/2020.
+//
+
+#ifndef BIUADVANEDPROG1_MS_2__SERVER_H_
+#define BIUADVANEDPROG1_MS_2__SERVER_H_
+
+class Server {
+
+};
+
+#endif //BIUADVANEDPROG1_MS_2__SERVER_H_
